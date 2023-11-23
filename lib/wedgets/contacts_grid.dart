727f60eaaa9c06872bf2cbe261b3ed7c1692209f
mahmoudbesho <nbesho63@gmail.com>
2023-11-23 17:10:32 +0200

    modify grid

---
+++ lib/wedgets/contacts_grid.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'social_Media_Icon.dart';
+class ContactGrid extends StatelessWidget {
+  const ContactGrid({
+    super.key,
+    required this.socialMedia,
+  });
+
+  final Map<String, String> socialMedia;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: socialMedia.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3),
+      itemBuilder: (mycontext, index) {
+        return SocialMediaIcon(
+          socialMedia: socialMedia.keys.toList()[index],
+          socialMediaLink: socialMedia.values.toList()[index],
+        );
+      },
+      shrinkWrap: true,
+      padding: EdgeInsets.all(8),
+    );
+  }
+}
